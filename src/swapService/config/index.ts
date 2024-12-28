ddb24b6a3ce09a6d830ab0b4d8827e5f2fda4b82
dglowinski <dariushg32111@gmail.com>
2024-12-28 13:16:49 +0100

    fix: swell config

--- src/swapService/config/index.ts
+++ src/swapService/config/index.ts
@@ -3,10 +3,12 @@ import type { RoutingConfig } from "../interface"
 import baseRoutingConfig from "./base"
 import defaultRoutingConfig from "./default"
 import mainnetRoutingConfig from "./mainnet"
+import swellRoutingConfig from "./swell"
 
 const routingConfig: RoutingConfig = {
   [mainnet.id]: mainnetRoutingConfig,
   [base.id]: baseRoutingConfig,
+  [1923]: swellRoutingConfig,
 }
 
 export const getRoutingConfig = (chainId: number) => {
