aa9242dd9461304f391d72aad0e5e2fd99b4fc87
dglowinski <dariushg32111@gmail.com>
2025-01-03 13:22:58 +0100

    feat: improve error logs

--- src/api/routes/swap/swapRouter.ts
+++ src/api/routes/swap/swapRouter.ts
@@ -70,7 +70,7 @@ async function findSwap(
 
     return ServiceResponse.success<SwapResponse>(data)
   } catch (error) {
-    console.log("error: ", error.statusCode, req.url)
+    console.log("error: ", error.statusCode, error.message, req.url)
     if (error instanceof ApiError) {
       return ServiceResponse.failure(
         error.message,
