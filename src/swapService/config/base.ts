400253f9a48481910a2446a449368d54e1f20a05
dglowinski <dariushg32111@gmail.com>
2025-01-20 21:35:27 +0100

    update base config

--- src/swapService/config/base.ts
+++ src/swapService/config/base.ts
@@ -2,6 +2,7 @@ import { type ChainRoutingConfig, SwapperMode } from "../interface"
 import { StrategyBalmySDK, StrategyRepayWrapper } from "../strategies"
 
 const CBBTC_BASE = "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf"
+const USDC_BASE = "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913"
 
 const baseRoutingConfig: ChainRoutingConfig = [
   // WRAPPERS
@@ -24,6 +25,18 @@ const baseRoutingConfig: ChainRoutingConfig = [
     },
     match: {
       tokensInOrOut: [CBBTC_BASE],
+    },
+  },
+  // avoid USDC on kyberswap
+  {
+    strategy: StrategyBalmySDK.name(),
+    config: {
+      sourcesFilter: {
+        includeSources: ["odos", "1inch", "open-ocean", "uniswap"],
+      },
+    },
+    match: {
+      tokensInOrOut: [USDC_BASE],
     },
   },
   // DEFAULTS
