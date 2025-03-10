b44b7223dced200a73dcbf4326246956c0747260
dglowinski <dariushg32111@gmail.com>
2025-03-10 19:24:04 +0100

    update config

--- src/swapService/config/mainnet.ts
+++ src/swapService/config/mainnet.ts
@@ -51,7 +51,6 @@ const mainnetRoutingConfig: ChainRoutingConfig = [
     strategy: StrategyERC4626Wrapper.name(),
     match: {
       tokensInOrOut: [
-        SUSDS_MAINNET,
         WSTUSR_MAINNET,
         PT_WSTUSR1740182579,
         YNETH_MAINNET,
